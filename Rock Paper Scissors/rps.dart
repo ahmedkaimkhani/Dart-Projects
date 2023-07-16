fe53a48f7df9237c87ef38b83de26cdd8ba34ae2
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-16 15:12:51 +0500

    rps

--- Rock Paper Scissors/rps.dart
+++ Rock Paper Scissors/rps.dart
@@ -1,11 +1,25 @@
 // Rock Paper Scissors
 import 'dart:io';
 
+enum move {rock, paper, scissors}
 void main (){
   while (true){
     print('== Welcome to Rock, Paper, Scissors ==');
   stdout.write('Press: r,p,s for Quit: q -----> ');
   var userInput = stdin.readLineSync();
   // selecting player move
+  var playerMove;
+
+  if (userInput == 'r'){
+    playerMove = move.rock;
+  } else if (userInput == 'p'){
+    playerMove = move.paper;
+  } else if (userInput == 's'){
+    playerMove = move.scissors;
+  } else if (userInput == 'q'){
+    break;
+  } else {
+    print('Invalid input');
+  }
   }
 }
